c181ccb444183267b5dd032e26d3b7eeef0c2bb1
Barucee <bruce.ravey@live.fr>
2022-11-23 15:22:05 +0100

    filter

--- Machine Learning.R
+++ Machine Learning.R
@@ -1,6 +1,58 @@
-#Creation of the Data Set
+# Import Libraries
 library("readxl")
+library("dplyr")
+require(dplyr)
+require(rlang)
 
-outputLaevenAndValencia <- read_excel("./Laeven and Valencia, 2013 and 2018.xlsx", sheet = 2)
 
-WorldBankData <- read_excel("./WB data.xlsx")
+# Creation of the Data Set
+outputLaevenAndValenciaRaw <- read_excel("./Laeven and Valencia, 2013 and 2018.xlsx", sheet = 2)
+WorldBankDataRaw <- read_excel("./WB data.xlsx")
+
+# Change some column name
+WorldBankDataRaw <- WorldBankDataRaw %>% 
+                                      rename(Country = `Country Name`)
+
+
+#Filter Data set
+AdvancedCountry <- c("Australia"
+                     , "Austria"
+                     , "Belgium"
+                     , "Canada"
+                     , "Czech Republic"
+                     , "Czechia"
+                     , "Denmark"
+                     , "Estonia"
+                     , "Finland"
+                     , "France"
+                     , "Germany"
+                     , "Greece"
+                     , "China, P.R.: Hong Kong"
+                     , "Hong Kong SAR, China"
+                     , "Iceland"
+                     , "Ireland"
+                     , "Israel"
+                     , "Italy"
+                     , "Japan"
+                     , "Korea"
+                     , "Korea, Rep."
+                     , "Latvia"
+                     , "Lithuania"
+                     , "Luxembourg"
+                     , "Netherlands"
+                     , "New Zealand"
+                     , "Norway"
+                     , "Portugal"
+                     , "Singapore"
+                     , "Slovak Republic"
+                     , "Slovenia"
+                     , "Spain"
+                     , "Sweden"
+                     , "Switzerland"
+                     , "United Kingdom"
+                     , "United States")
+
+#attention Hong Kong avec une rédaction special
+outputLaevenAndValenciaAdvanced <- filter(outputLaevenAndValenciaRaw, Country %in% AdvancedCountry)
+WorldBankDataAdvanced <- filter(WorldBankDataRaw, Country %in% AdvancedCountry)
+
