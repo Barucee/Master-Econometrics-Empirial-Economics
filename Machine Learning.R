bc886c4f97155db47f12ae2cdacbffa029861e23
emamusco <118898817+emamusco@users.noreply.github.com>
2022-11-29 23:12:40 +0100

    Update Machine Learning.R

--- Machine Learning.R
+++ Machine Learning.R
@@ -11,20 +11,8 @@ library(purrr)
 # Creation of the Data Set
 outputLaevenAndValenciaRaw <- read_excel("./Laeven and Valencia, 2013 and 2018.xlsx", sheet = 2)
 WorldBankDataRaw <- read_excel("./WB data.xlsx", na = "..")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-WorldBankexternernaldebtRaw <- read_excel("./WB data external debt.xlsx")
->>>>>>> be992c07156540892f7c2164e01c56efd34cacb3
-=======
->>>>>>> 76b454398afa27470108fce68061ad7c5fc354db
 IMFPublicDebtToGDP <- read_excel("./IMF - Public Debt-to-GDP.xls", na="no data")
 
-#PB OpennesIndexWB
-## !
-OpennessIndexWB <- read.csv("./OpennessIndexWB.csv", sep =",")
-## !
-#PB OpennesIndexWB
 
 
 # Change some column name
@@ -87,25 +75,10 @@ outputLaevenAndValenciaAdvanced <- filter(outputLaevenAndValenciaRaw, Country %i
 WorldBankDataAdvanced <- filter(WorldBankDataRaw, Country %in% AdvancedCountry)
 IMFPublicDebtToGDPAdvanced <- filter(IMFPublicDebtToGDP, Country %in% AdvancedCountry) #Pas Hong Kong
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 76b454398afa27470108fce68061ad7c5fc354db
-
 # Transform all from wide to long
 IMFPublicDebtToGDPAdvanced <- gather(IMFPublicDebtToGDPAdvanced, year, "Public Debt To GDP", "1950":"2020", factor_key=FALSE)
 
 
-#Merging 
-IMFPublicDebtToGDPAdvanced <- IMFPublicDebtToGDPAdvanced %>% 
-  rename ( Year = year) 
-IMFPublicDebtToGDPAdvanced$Year<- as.numeric(IMFPublicDebtToGDPAdvanced$Year)
-IMFPublicDebtToGDPAdvanced$Country<- as.array(IMFPublicDebtToGDPAdvanced$Country)
-
-WorldBankDataAdvanced$Country <- as.array(WorldBankDataAdvanced$Country)
-df<- merge(IMFPublicDebtToGDPAdvanced,WorldBankDataAdvanced)
-df$openness_index <- df$Exports + df$Imports
-df <- select(df, -c(`External debt`,`Exports`,`Imports`,`Public debt`,`Bank capital to assets ratio (%)`) )
 
 
 #Merging 
@@ -121,11 +94,7 @@ df <- select(df, -c(`External debt`,`Exports`,`Imports`,`Public debt`,`Bank capi
 
 
 
-#coding crises
-outputLaevenAndValenciaAdvanced <- outputLaevenAndValenciaAdvanced[,c(1,2)]
-outputLaevenAndValenciaAdvanced <- outputLaevenAndValenciaAdvanced %>% 
-  separate_rows("Systemic Banking Crisis (starting date)", sep=",") %>% 
-  separate("Systemic Banking Crisis (starting date)", into=c("Crisis"),sep = "_", convert = TRUE)
+
 
 
 
